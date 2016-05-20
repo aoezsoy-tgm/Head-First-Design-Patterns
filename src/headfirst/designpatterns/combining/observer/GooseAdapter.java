b32f8eeae07b849fa01e5d87d8fc99e0ee1de212
Ahmet Oezsoy <aoezsoy@student.tgm.ac.at>
2016-05-20 16:53:00 +0200

    finished javadoc for the class GooseAdapter

--- src/headfirst/designpatterns/combining/observer/GooseAdapter.java
+++ src/headfirst/designpatterns/combining/observer/GooseAdapter.java
@@ -1,27 +1,56 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * GooseAdapter - Eine Klasse das von Quackable implementiert.
+ * Zugehörigkeit: Adapter Pattern
+ * Beschreibung allgemein für alle Klassen, die zu Adapter Pattern
+ * gehören : 
+ * Eine Gans kann durch die GooseAdapter-Klasse auch als Quackable angesehen werden, 
+ * da die quack()-Methode die honk()-Methode aufruft.
+ * @author Oezsoy Ahmet
+ * @version 20.05.2016
+ */
 public class GooseAdapter implements Quackable {
 	Goose goose;
 	Observable observable;
 
+	/**
+	 * Hier wird beim Konstruktor eine Zuweisung durchgeführt und 
+	 * eine neue Instanz für Observable erzeugt.
+	 */
 	public GooseAdapter(Goose goose) {
 		this.goose = goose;
 		observable = new Observable(this);
 	}
  
+	/**
+	 * Bei dieser Methode wird die Methode honk aufgerufen und
+	 * der Observer benachrichtigt.
+	 */
 	public void quack() {
 		goose.honk();
 		notifyObservers();
 	}
 
+	/**
+	 *  @param observer 
+	 *  In dieser Methode wird die Methode registerObserver mit dem Parameter observer aufgerufen. 
+	 *  Die Methode registerOberserver befindet sich in observable.
+	 */
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
 
+	/**
+	 *  Diese Methode ruft die Methode notifyObservers von der Klasse Observable auf.
+	 */
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
 
+	/**
+	 *  Diese Methode ist ein String und liefert 'Goose pretending to be a Duck' zurück.
+	 */
 	public String toString() {
 		return "Goose pretending to be a Duck";
 	}
