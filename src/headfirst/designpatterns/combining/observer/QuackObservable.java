3d29c721c80a3fddf7026f8a5b346775b36ded6f
Ahmet Oezsoy <aoezsoy@student.tgm.ac.at>
2016-05-20 16:36:39 +0200

    finished javadoc for the class QuackObservable

--- src/headfirst/designpatterns/combining/observer/QuackObservable.java
+++ src/headfirst/designpatterns/combining/observer/QuackObservable.java
@@ -1,6 +1,17 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * QuackObservable - QuackObservable ist ein Interface mit 2 Methoden.
+ * @author Oezsoy Ahmet
+ * @version 20.05.2016
+ */
 public interface QuackObservable {
+	/**
+	 * Die Methode hat einen Parameter mit dem Typen observer
+	 */
 	public void registerObserver(Observer observer);
+	/**
+	 * Die Methode dient zur Benachrichtigung für den Observer.
+	 */
 	public void notifyObservers();
 }
