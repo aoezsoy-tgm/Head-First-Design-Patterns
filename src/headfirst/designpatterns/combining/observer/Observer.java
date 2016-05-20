e7e057420988ee2c6a5f0bd0efffa4f3ce174670
Ahmet Oezsoy <aoezsoy@student.tgm.ac.at>
2016-05-20 16:28:25 +0200

    finished javadoc for the class Observer

--- src/headfirst/designpatterns/combining/observer/Observer.java
+++ src/headfirst/designpatterns/combining/observer/Observer.java
@@ -1,5 +1,13 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * Observer - Observer ist ein Interface.
+ * @author Oezsoy Ahmet
+ * @version 20.05.2016
+ */
 public interface Observer {
+	/**
+	 * Bei dieser Methode update wird QuackObservable mit dem Typ duck definiert..
+	 */
 	public void update(QuackObservable duck);
 }
