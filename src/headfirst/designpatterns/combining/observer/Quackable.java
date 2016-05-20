9e47e2d83a80143c94d38e47a3ea66ada292cd93
Ahmet Oezsoy <aoezsoy@student.tgm.ac.at>
2016-05-20 15:40:34 +0200

    finished javadoc for the class Quackable

--- src/headfirst/designpatterns/combining/observer/Quackable.java
+++ src/headfirst/designpatterns/combining/observer/Quackable.java
@@ -1,5 +1,12 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * Quackable - Quakable erbt von QuackObservable und hat eine Methode quack.
+ * Diese Methode steht zur Verfügung damit man bei den anderen Duckklassen die Methode
+ * verwenden bzw. überschreiben können.
+ * @author Oezsoy Ahmet
+ * @version 20.05.2016
+ */
 public interface Quackable extends QuackObservable {
 	public void quack();
 }
