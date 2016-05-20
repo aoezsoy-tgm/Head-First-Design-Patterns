cc37ea6a789a703e57b911dcaf5779a2a730cc23
Ahmet Oezsoy <aoezsoy@student.tgm.ac.at>
2016-05-20 17:00:04 +0200

    finished javadoc for the class Flock

--- src/headfirst/designpatterns/combining/observer/Flock.java
+++ src/headfirst/designpatterns/combining/observer/Flock.java
@@ -3,13 +3,31 @@ package headfirst.designpatterns.combining.observer;
 import java.util.Iterator;
 import java.util.ArrayList;
 
+/**
+ * Flock - Diese Klasse Implementiert das Interface Quackable.
+ * @author Oezsoy Ahmet
+ * @version 20.05.2016
+ */
 public class Flock implements Quackable {
+	// Klassen ArrayList ducks mit dem Typen Quackable
 	ArrayList<Quackable> ducks = new ArrayList<Quackable>();
   
+	/**
+	 * Methoden Name: add
+	 * Diese Methode hat einen Parameter mit dem Tyen Quackable mit dem namen duck
+	 * Dieser duck wird in die Arraylist hinzugefügt.
+	 */
+	
 	public void add(Quackable duck) {
 		ducks.add(duck);
 	}
-  
+  	
+	/**
+	 *	Methoden Name: quack
+	 *	Hier befindet sich ein Iterator mit dem man alle elemente vom Arraylist durchgehen kann und dies auch gemacht wird.
+	 *	Es wird noch dazu überprüft.
+	 *	Es hat außerdem noch einen Parameter verlangt, dammit es etwas adden kann.
+	 */
 	public void quack() {
 		Iterator<Quackable> iterator = ducks.iterator();
 		while (iterator.hasNext()) {
@@ -17,7 +35,13 @@ public class Flock implements Quackable {
 			duck.quack();
 		}
 	}
-   
+	
+   	/**
+	 *	Methoden Name: registerObserver
+	 *	Hier befindet sich ein Iterator mit dem man alle elemente vom Arraylist durchgehen kann und dies auch gemacht wird.
+	 *	Es wird noch dazu überprüft.
+	 *	Es hat außerdem noch einen Parameter verlangt, dammit es etwas adden kann.
+	 */
 	public void registerObserver(Observer observer) {
 		Iterator<Quackable> iterator = ducks.iterator();
 		while (iterator.hasNext()) {
@@ -25,9 +49,12 @@ public class Flock implements Quackable {
 			duck.registerObserver(observer);
 		}
 	}
-  
 	public void notifyObservers() { }
-  
+	
+	/**
+	 *	Methoden Name: toString
+	 *	Rückgabe: String: 'Flock of Ducks'
+	 */
 	public String toString() {
 		return "Flock of Ducks";
 	}
