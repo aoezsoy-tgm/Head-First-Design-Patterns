b40c2a3e64350e26e1b87832371b4bb17e9185bb
Ahmet Oezsoy <aoezsoy@student.tgm.ac.at>
2016-05-20 16:11:12 +0200

    finished javadoc for the class Observable

--- src/headfirst/designpatterns/combining/observer/Observable.java
+++ src/headfirst/designpatterns/combining/observer/Observable.java
@@ -3,18 +3,40 @@ package headfirst.designpatterns.combining.observer;
 import java.util.Iterator;
 import java.util.ArrayList;
 
+/**
+ * Observable - Eine Klasse die vom Interface QuackObservable implementiert.
+ * Zugehörigkeit: Observer Pattern
+ * Beschreibung allgemein für alle Klassen, die zu Observer Pattern
+ * gehören : 
+ * Die Klasse Quackologist ist die einzige Observer (Beobachter)-Klasse. 
+ * Alle Enten werden zur Observable-Klasse referenziert,welche die Quackologist-Klasse 
+ * mittels der Methode update() benachrichtigt.
+ * @author Oezsoy Ahmet
+ * @version 20.05.2016
+ */
 public class Observable implements QuackObservable {
-	ArrayList<Observer> observers = new ArrayList<Observer>();
+	
+	ArrayList<Observer> observers = new ArrayList<Observer>(); // ArrayList für observer
 	QuackObservable duck;
  
+	/**
+	 * Beim Konstruktor wird duck zugewiesen.
+	 */
 	public Observable(QuackObservable duck) {
 		this.duck = duck;
 	}
   
+	/**
+	 *  @param observer 
+	 *  In dieser Methode werden Elemente in die ArrayList hinzugefügt.
+	 */
 	public void registerObserver(Observer observer) {
 		observers.add(observer);
 	}
   
+	/**
+	 * Bei dieser Methode werden alle Elemente durchgegangen mithilfe vom Iterator.
+	 */
 	public void notifyObservers() {
 		Iterator<Observer> iterator = observers.iterator();
 		while (iterator.hasNext()) {
@@ -23,7 +45,10 @@ public class Observable implements QuackObservable {
 		}
 	}
  
+	/**
+	 * Eine getter-Methode, dass Observer mit dem Iterator zurückliefert.
+	 */
 	public Iterator<Observer> getObservers() {
 		return observers.iterator();
 	}
-}
+}       
