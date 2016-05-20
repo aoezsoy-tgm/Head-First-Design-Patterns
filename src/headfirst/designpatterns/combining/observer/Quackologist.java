f3a1ac942b2c4071f26ecfa68f29ff52d473f2ef
Ahmet Oezsoy <aoezsoy@student.tgm.ac.at>
2016-05-20 16:24:19 +0200

    finished javadoc for the class Quackologist

--- src/headfirst/designpatterns/combining/observer/Quackologist.java
+++ src/headfirst/designpatterns/combining/observer/Quackologist.java
@@ -1,11 +1,28 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * Quackologist - Eine Klasse die vom Interface Observer implementiert.
+ * Zugehörigkeit: Observer Pattern
+ * Beschreibung allgemein für alle Klassen, die zu Observer Pattern
+ * gehören : 
+ * Die Klasse Quackologist ist die einzige Observer (Beobachter)-Klasse. 
+ * Alle Enten werden zur Observable-Klasse referenziert,welche die Quackologist-Klasse 
+ * mittels der Methode update() benachrichtigt.
+ * @author Oezsoy Ahmet
+ * @version 20.05.2016
+ */
 public class Quackologist implements Observer {
  
+	/**
+	 * Bei dieser Methode update wird duck ausgegeben.
+	 */
 	public void update(QuackObservable duck) {
 		System.out.println("Quackologist: " + duck + " just quacked.");
 	}
  
+	/**
+	 * Eine toString-Methode, dass ein String ausgibt.
+	 */
 	public String toString() {
 		return "Quackologist";
 	}
