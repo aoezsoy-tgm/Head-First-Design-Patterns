ac7621339b61e006bf4d98ac70541b2f5fa8afde
Ahmet Oezsoy <aoezsoy@student.tgm.ac.at>
2016-05-20 16:52:43 +0200

    finished javadoc for the class Goose

--- src/headfirst/designpatterns/combining/observer/Goose.java
+++ src/headfirst/designpatterns/combining/observer/Goose.java
@@ -1,11 +1,27 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * Goose - Eine Klasse das zwei Methoden zur Verfügung stellt.
+ * Zugehörigkeit: Adapter Pattern
+ * Beschreibung allgemein für alle Klassen, die zu Adapter Pattern
+ * gehören : 
+ * Eine Gans kann durch die GooseAdapter-Klasse auch als Quackable angesehen werden, 
+ * da die quack()-Methode die honk()-Methode aufruft.
+ * @author Oezsoy Ahmet
+ * @version 20.05.2016
+ */
 public class Goose {
 
+	/**
+	 * Eine Methode, dass Honk in der Konsole ausgibt.
+	 */
 	public void honk() {
 		System.out.println("Honk");
 	}
 
+	/**
+	 * Eine toString-Methode, dass ein String zurückliefert.
+	 */
 	public String toString() {
 		return "Goose";
 	}
