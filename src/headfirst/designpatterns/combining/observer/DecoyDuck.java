284738f25fd32e3169bc0bf86de19eebd5f7f443
Ahmet Oezsoy <aoezsoy@student.tgm.ac.at>
2016-05-20 16:23:53 +0200

    finished javadoc for the class DecoyDuck

--- src/headfirst/designpatterns/combining/observer/DecoyDuck.java
+++ src/headfirst/designpatterns/combining/observer/DecoyDuck.java
@@ -1,25 +1,59 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * DecoyDuck - Eine Klasse die von dem Interface Quakable implementiert. Das heißt
+ * es enthält alle Attribute und Methoden von Quakable.
+ * Zugehörigkeit: Factory Pattern(Factory Method)
+ * Beschreibung allgemein für alle Klassen, die zu Factory Pattern(Factory Method) 
+ * gehören : 
+ * Die Enten werden jeweils von den Klassen DuckFactory und CountingDuckFactory 
+ * instanziert und werden als Quackable zurückgegeben. Die Klasse CountingDuckFactory 
+ * erweitert die Enten mit einem QuackCounter und gibt diesen zurück. 
+ * Es werden dabei keine Methoden von der Klasse AbstractDuckFactory implementiert 
+ * und aus diesem Grund ist es keine Abstract Factory, sondern ein Factory Method.
+ * @author Oezsoy Ahmet
+ * @version 20.05.2016
+ */
 public class DecoyDuck implements Quackable {
 	Observable observable;
 
+	/**
+	 * Diese Methode erstellt eine neue Instanz von Observable mit 
+	 * dem Parameter Observable.
+	 */
 	public DecoyDuck() {
 		observable = new Observable(this);
 	}
  
+	/**
+	 * Bei dieser Methode wird ein String ausgegeben und 
+	 * danach ruft es die Methode notifyObserver auf.
+	 */
 	public void quack() {
 		System.out.println("<< Silence >>");
 		notifyObservers();
 	}
  
+	/**
+	 *  @param observer 
+	 *  In dieser Methode wird die Methode registerObserver mit dem Parameter observer aufgerufen. 
+	 *  Die Methode registerOberserver befindet sich in observable.
+	 */
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
 
+	/**
+	 *  Diese Methode ruft die Methode notifyObservers von der Klasse Observable auf.
+	 */
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
  
+	/**
+	 *  Diese Methode ist ein String und gibt den Namen der Ente 
+	 *  als String aus: 'Decoy Duck'.
+	 */
 	public String toString() {
 		return "Decoy Duck";
 	}
